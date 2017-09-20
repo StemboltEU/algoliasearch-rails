4dfbeeda5d4e8b0cffe7e084caa275488ec541d5
Sylvain UTARD <sylvain.utard@gmail.com>
2017-09-20 16:05:22 +0200

    Sequel deprecated then removed the Model.after_commit method.

--- lib/algoliasearch-rails.rb
+++ lib/algoliasearch-rails.rb
@@ -410,7 +410,7 @@ module AlgoliaSearch
           class_eval do
             copy_after_validation = instance_method(:after_validation)
             copy_before_save = instance_method(:before_save)
-            copy_after_commit = instance_method(:after_commit)
+            copy_after_save = instance_method(:after_save)
 
             define_method(:after_validation) do |*args|
               super(*args)
@@ -424,9 +424,9 @@ module AlgoliaSearch
               super(*args)
             end
 
-            define_method(:after_commit) do |*args|
+            define_method(:after_save) do |*args|
               super(*args)
-              copy_after_commit.bind(self).call
+              copy_after_save.bind(self).call
               algolia_perform_index_tasks
             end
           end
